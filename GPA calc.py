cd6f06f4c8dcbf825a89e52ccabf88dfcc92b58a
BHile-spec <bhile5@ivytech.edu>
2024-06-24 14:18:29 -0400

    Update GPA calc.py

--- GPA calc.py
+++ GPA calc.py
@@ -1,5 +1,6 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+#Brian Hile
+#GPA calc
+#calculates gpa through user input
 >>> def main():
 ...     while True:
 ...         last_name = input("Enter student's last name (Enter 'ZZZ' to quit): ")
